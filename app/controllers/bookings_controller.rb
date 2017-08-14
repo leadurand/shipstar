5e0b9afd4d76406ec84d93b97f1d6dce9f937752
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-14 20:54:41 +0200

    Suppressed strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,10 +2,4 @@ class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
   end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:start_at, :end_at, :user_id, :ship_id)
-  end
 end
