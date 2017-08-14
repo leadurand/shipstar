468f133c887d96a7a0125c0f914a187b44393222
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-14 20:56:16 +0200

    Delete strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,10 +15,4 @@ class BookingsController < ApplicationController
 
     redirect_to bookings_path
   end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:start_at, :end_at, :user_id, :ship_id)
-  end
 end
