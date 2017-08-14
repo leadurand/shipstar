bd6c37fb378e48e7612403e932f1d7f4f3b95e03
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-14 17:49:08 +0200

    Implemented bookings#show with view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_at, :end_at, :user_id, :ship_id)
+  end
 end
