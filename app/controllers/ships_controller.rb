6edf1461635991a275fdfe6c23601292e64b957d
clement bergantz <bergantz.clement@gmail.com>
2017-08-15 18:16:22 +0200

    add link between index and show

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -3,12 +3,19 @@ class ShipsController < ApplicationController
 	  @ship = Ship.find(params[:id])
 	  @booking = Booking.new
 	end
+	
 	def index
-		# get the query string from the params and do the search
-		@address = params[:city]
-		@ships_info = params[:category]
-		@ship_class = ShipsInfo.find(@ships_info).ship_class
-		@ships = Ship.joins(:ships_info).where("address LIKE ?", "%#{@address}%").where("ships_infos.ship_class LIKE ?", "%#{@ship_class}%")
+		@ships = if params[:city]
+			# get the query string from the params
+			@address = params[:city]
+			@ships_info = params[:category]
+			# access the ship_class from the ShipsInfo model
+			@ship_class = ShipsInfo.find(@ships_info).ship_class
+			# query the database
+			@ships = Ship.joins(:ships_info).where("address LIKE ?", "%#{@address}%").where("ships_infos.ship_class LIKE ?", "%#{@ship_class}%")
+		else
+			Ship.all
+		end
 	end
 
 	private
