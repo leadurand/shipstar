e7fe392d1e34fac1bcebe3e6c758ba5a061dc63f
Romain Neuville <neuvilleromain@gmail.com>
2017-08-15 15:29:26 +0200

    Facebook ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :ships, only: [:index, :show], shallow: true do
     resources :bookings, only: [:show, :destroy, :create]
