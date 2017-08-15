ad9b112fac761cbea0eb29fad5cc5c3874432ad4
Lea Durand <leadurand.pro@outlook.com>
2017-08-15 14:19:55 +0200

    changed validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
   has_many :ships # => his own ships
-  # validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   # # ajouter collection pour :species & :planet => seed
   # # validates :species, inclusion: { in: ['Anx', 'Humain', 'Jedi', 'Ewok', 'Droid'], allow_nil: false }
   # # validates :planet, presence: true, inclusion: { in: ['Tatawin', 'Pipada', 'Kamino', 'Dagobah'], allow_nil: false }
-  # belongs_to :specie
-  # belongs_to :planet
+  belongs_to :specie
+  belongs_to :planet
 end
