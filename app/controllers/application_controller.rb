3fc7d72dffd48c9041c79286b1c63c955bfeb195
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-15 13:38:45 +0200

    Added devise config + signup entries(name, planet, specie)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :specie_id, :planet_id])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :specie, :planet])
+  end
 end
