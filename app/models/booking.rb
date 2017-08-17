9413c85cc25d4a64e3d5dd2bf80b5f1a8dedf07e
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-17 18:57:40 +0200

    validation-booking-over-1-day

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,7 @@ class Booking < ApplicationRecord
   validates :end_at, presence: true, :uniqueness => { :scope => [:end_at, :start_at] }
   validates :ship_id, presence: true
   validates :user_id, presence: true
-  validate :start_at_cannot_be_in_the_past, :end_at_must_be_greater_than_start_at
+  validate :start_at_cannot_be_in_the_past, :end_at_must_be_greater_than_start_at, :length_must_be_over_one_day
 
   def start_at_cannot_be_in_the_past
     if start_at.present? && start_at < Date.today
@@ -18,7 +18,13 @@ class Booking < ApplicationRecord
       errors.add(:end_at, "End date should be greater than Start date!")
     end
   end
-  
+
+  def length_must_be_over_one_day
+    if self.length < 1
+      errors.add(:end_at, "Your booking should be over one day!")
+    end
+  end
+
   def length
     (self.end_at - self.start_at) / 86400
   end
