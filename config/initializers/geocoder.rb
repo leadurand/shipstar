9a29d625a0d777091f35f019eff5d2bacf4d732f
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-17 15:53:39 +0200

    fixed geocoder config

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
-  timeout: 15,                 # geocoding service timeout (secs)
+  #timeout: 15,                 # geocoding service timeout (secs)
   lookup: :google,            # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: ENV['GOOGLE_API_SERVER_KEY'],               # API key for geocoding service
+  api_key: ENV['GOOGLE_API_SERVER_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
