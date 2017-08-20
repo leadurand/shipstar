d8792410166b11db8f5496921dfd19457f4d2fa3
simon guillorit <simon.guillorit@gmail.com>
2017-08-20 23:02:32 +0200

    displaying well rated ships on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,16 @@ class PagesController < ApplicationController
   	@users = User.all
     @bookings = Booking.all
     @ship = Ship.new
-  	@ships = Ship.all.last(3)
+    # @ships is used to display 3 ships on home page
+    # actually not the best way to do it. I should find it with mean_rating method
+    # but I cannot access it with ActiveRecord methods. The best solution I found
+    # was to find 3 bookings rated 5/5 and select associated ships.
+    # It works fine because we have only a few bookings seeded (~100) so there is
+    # good chance that a ship that once received a 5/5 still has a great mean_rating.
+    @ships = []
+  	Booking.where('rating = 5').first(3).each do |booking|
+      @ships << booking.ship
+    end
     @ships_all = Ship.all
     @hash = Gmaps4rails.build_markers(@ships_all) do |ship, marker|
          marker.lat ship.latitude
